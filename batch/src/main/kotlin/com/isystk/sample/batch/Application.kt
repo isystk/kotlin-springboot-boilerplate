05b9199e36900ef6515f80b26d034b38ede87af5
iseyoshitaka <ise0615@gmail.com>
2021-01-10 10:02:09 +0900

    Batchが起動しないので修正

--- batch/src/main/kotlin/com/isystk/sample/batch/Application.kt
+++ batch/src/main/kotlin/com/isystk/sample/batch/Application.kt
@@ -3,22 +3,23 @@ package com.isystk.sample.batch
 import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.WebApplicationType
+import com.isystk.sample.ComponentScanBasePackage
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
-object Application {
-    private val log = LoggerFactory.getLogger(Application::class.java)
+@SpringBootApplication(scanBasePackageClasses = [ComponentScanBasePackage::class])
+class Application
 
-    @Throws(Exception::class)
-    @JvmStatic
-    fun main(args: Array<String>) {
-        try {
-            val application = SpringApplication(Application::class.java)
-            application.webApplicationType = WebApplicationType.NONE
-            val context = application.run(*args)
-            val exitCode = SpringApplication.exit(context)
-            System.exit(exitCode)
-        } catch (t: Throwable) {
-            log.error("failed to run. ", t)
-            System.exit(1)
-        }
+private val log = LoggerFactory.getLogger(Application::class.java)
+
+fun main(args: Array<String>) {
+    try {
+        val application = SpringApplication(Application::class.java)
+        application.webApplicationType = WebApplicationType.NONE
+        val context = application.run(*args)
+        val exitCode = SpringApplication.exit(context)
+        System.exit(exitCode)
+    } catch (t: Throwable) {
+        log.error("failed to run. ", t)
+        System.exit(1)
     }
 }
